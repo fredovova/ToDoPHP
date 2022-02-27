e0efd4553b3cf065e66a4ae65c2d98878a7b550a
_fredo_v <filyukhinv@bk.ru>
2022-02-27 14:29:34 +0300

    sessia

--- about.php
+++ about.php
@@ -1,14 +1,24 @@
 <?php
-$task=$_POST['task']; 
-if($task==''){
-    echo "Введите само задание ";
-    exit();
+session_start();
+
+unset($_SESSION['task']);
+unset($_SESSION['error']);
+$task=$_POST['task'] ;
+$_SESSION['task']=$task;
+$_SESSION['error']='';
+if(strlen($task)<=1){
+  $_SESSION['error']='Введите само задание ';
+  header('location: /');
 }
 
+else if ((strlen($task)>=1)){
+ 
 require 'configBD.php';
-  $mysql->query("INSERT INTO `tasks`(`tasks`)
-  VALUES('$task')");
+  $mysql->query("INSERT INTO `tasks`(`tasks`) VALUES('$task')");
 
   $mysql->close();
 header('location: /');
+
+}
+
 ?>
--- index.php
+++ index.php
@@ -1,21 +1,29 @@
 <!DOCTYPE html>
+<?php
+session_start();
+
+?>
 <html lang="ru">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Список дел</title>
+    <title>Список Дел</title>
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
-</head>
-<body>
+    <body>
     <div class="container">
+
         <h1>Список Дел </h1>
     <form action="/about.php" method="POST">
-    <input type="text" name="task" id="task" placeholder="Нужно сделать .." class="form-control">
+    <input type="text" name="task" id="task"  placeholder="Нужно сделать .." class="form-control">
+   <p>
+       <br>
+     <?='<p>'. $_SESSION['error'].'</p>';  ?>
+        </P>
     <button type="submit" name="sendTask" class="btn btn-success"> Отправить </button>
     </form>
-    
+   
     <?php
     require 'configBD.php';
     
@@ -28,5 +36,5 @@
 
     echo '</ul>'
     ?>
-</body>
+ </body>
 </html>
